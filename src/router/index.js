e0a8512fb5ead813d9970fe0b3368ae9a40131d3
Wells <chawkeye@gmail.com>
2018-07-30 15:00:59 +0800

    套用bootstrap dashboard版型

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/pages/Login'
 import HelloWorld from '@/components/HelloWorld'
+import DashBoard from '@/components/DashBoard'
+import Products from '@/components/pages/Products'
 
 Vue.use(Router)
 
@@ -21,6 +23,19 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
-    }
+    },
+    {
+      path: '/admin',
+      name: 'DashBoard',
+      component: DashBoard,
+      children: [
+        {
+          path: 'products',
+          name: 'products',
+          component: Products,
+          meta: { requiresAuth: true }
+        }
+      ],
+    },
   ]
 })
