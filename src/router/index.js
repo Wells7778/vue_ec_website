870ab6a1f9b60708fea030a797e924112e19656d
Wells <chawkeye@gmail.com>
2018-07-30 14:09:11 +0800

    added Login.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/pages/Login'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
